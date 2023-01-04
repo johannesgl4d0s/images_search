d39e48e0bd0efa0c3038179f651dab2c5d0bb9f3
dejan <dejan.prv@gmail.com>
2023-01-04 13:55:53 +0100

    create index refactor

--- huggingface.py
+++ huggingface.py
@@ -6,14 +6,18 @@ import pickle
 import gc
 
 
-class HuggingFaceImagePredictor:
-    model_name: str = "google/vit-base-patch16-224"
-    index_file: str = "./data/index_hf.pickle"
+class HuggingFaceImageClassifier:
+    model_name = ""
+    index_file = ""
+    pipe = None
+    index = None
 
-    def __init__(self, model_name: str = None):
-        if model_name is not None:
-            self.model_name = model_name        
+    def __init__(self, model_name: str = "google/vit-base-patch16-224", 
+                 index_file: str = "./data/index_hf.pickle"):
+        self.model_name = model_name        
+        self.index_file = index_file
         self.pipe = pipeline(task="image-classification", model=self.model_name)
+        self.index = self.load_index(self.index_file)
 
         if Path(self.index_file).exists():
             self.index = self.load_index(self.index_file)
@@ -32,31 +36,39 @@ class HuggingFaceImagePredictor:
 
     def create_index(self, image_repo: str) -> None:
         images = list(Path(image_repo).iterdir())
-        index = dict()
+        data = list()
         for i, image in enumerate(images):
             print(f"Processing {image.name}")
             name = image.name
             features, top_class = self.extract_features(image.resolve().__str__())
-            index.update({"name": name, "top_class": top_class, "features": features})
+            data.append({"name": name, "top_class": top_class, "features": features})
 
-            if i == 10: 
+            if i % 2 == 0 and i != 0: 
+                print(f"Write index to {self.index_file}")                
+                index = self.load_index(self.index_file)
+                index = [list() if index is None else index]
+                with open(self.index_file, "wb") as f:
+                    index.append(data)
+                    pickle.dump(index, f, protocol=pickle.HIGHEST_PROTOCOL)
+                index = list()
+                data = list()
                 gc.collect()            # garbage collection
                 break
-
-        with open(self.index_file, "w") as f:
-            print(f"Write index to {self.index_file}")
-            pickle.dump(index, f, protocol=pickle.HIGHEST_PROTOCOL)
-        self.index = index
+        self.index = self.load_index(self.index_file)
 
     def load_index(self, index_file: str):
-        with open(index_file, "r") as f:
+        if Path(index_file).exists() == False:
+            print(f"Index file {index_file} not found. Please use create_index().")  
+            return None        
+        with open(index_file, "rb") as f:
             index = pickle.load(f)
         return index
+
 
     def cosine_similarity():
         pass
 
 
 if __name__ == "__main__":
-    clf = HuggingFaceImagePredictor()
+    clf = HuggingFaceImageClassifier()
     clf.create_index("./img/imagenet")
