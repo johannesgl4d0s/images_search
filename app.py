73851167b22ce6194b5e59ccd5ab9dfc332c6d3a
dejse <47352728+dejse@users.noreply.github.com>
2023-01-08 15:04:54 +0100

    Refactoring functions to class, update streamlit app (#4)
    * cleanup
    
    * refactor to class I (not finished)
    
    * added to dos
    
    * tf class almost finished
    
    * tf working
    
    * tf make methods private
    
    * added docstrings, and training nb for colab
    
    * update streamlit app with tf
    
    * fixed load_img, working streamlit
    
    * change from cache to singleton

--- app.py
+++ app.py
@@ -4,15 +4,26 @@ run with:
 $ python -m streamlit run app.py
 """
 import streamlit as st
-import matplotlib.pyplot as plt
 from PIL import Image
 
 
-# Cache Hugging Face model
-@st.experimental_memo
+# Make Singleton Hugging Face model
+@st.experimental_singleton
 def get_hugging_face_model():
     from huggingface import HuggingFaceImageClassifier
     return HuggingFaceImageClassifier(index_file="./data/index_hf_25k.pickle")
+
+# Make Singleton TensorFlow model
+# (Caching not working due to pickle issues with TF)
+@st.experimental_singleton
+def get_tensorflow_model():
+    from tf import KerasImageClassifier
+    return KerasImageClassifier(index_file="./data/index_tf.pickle", pca_file="./data/pca_tf.pickle")
+
+
+# Pre-Load both models
+clf_tf = get_tensorflow_model()             
+clf_hf = get_hugging_face_model()
 
 
 # Sidebar
@@ -20,8 +31,7 @@ st.sidebar.markdown("# Similar Image Search Engine")
 st.sidebar.markdown("Upload an image and find similar images in the database")
 uploaded_img = st.sidebar.file_uploader("Upload an image", type=["jpg", "png"])
 
-model_type = st.sidebar.radio("Model Type", ("Keras", "Hugging Face"))
-
+model_type = st.sidebar.radio("Model Type", ("Tensorflow", "Hugging Face"))
 
 # Display uploaded image
 st.markdown("## Uploaded Image")
@@ -30,20 +40,18 @@ if uploaded_img is not None:
     img = Image.open(uploaded_img)
     st.image(img, width=300)
 
-
 # Show similar images
 st.markdown("## Similar Images")
 st.markdown("The following images are similar to the uploaded image")
 
-with st.spinner("Loading Hugging Face model..."):
-    clf_hf = get_hugging_face_model()
-
 if uploaded_img is not None:
-    if model_type == "Hugging Face":
-        with st.spinner("Calculating Scores..."):
+    with st.spinner("Calculating Scores/Distances..."):
+        if model_type == "Tensorflow":
+            similar_images = clf_tf.find_similar_images(img, top_k=10)
+        else:
             similar_images = clf_hf.find_similar_images(img, top_k=10)
 
-            for image, score in similar_images:        
-                st.markdown(f"**{image}**")
-                st.markdown(f"**Score: {score:.2f}**")
-                st.image(f"./img/imagenet-mini/{image}", width=300)
+        for image, score in similar_images:        
+            st.markdown(f"**{image}**")
+            st.markdown(f"**Score/Distance: {score:.2f}**")
+            st.image(f"./img/imagenet-mini/{image}", width=300)
---
+++ tf.py
@@ -0,0 +1,193 @@
+import keras
+from keras.applications.imagenet_utils import preprocess_input
+from keras.models import Model
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+from scipy.spatial import distance
+
+from typing import Tuple, List, Union
+from PIL import Image
+from pathlib import Path
+import gc
+import pickle
+
+class KerasImageClassifier:
+    model = None
+    feature_extractor = None
+    index_file = ""
+    pca_file = ""
+    index = None
+    pca = None
+
+    def __init__(self, 
+                index_file: str = "./data/index_tf.pickle",
+                pca_file: str = "./data/pca_tf.pickle"):
+        """
+        Keras Image Classifier using TensorFlow backend. 
+        The model is trained on ImageNet dataset. Builds an index of images and finds similar images.
+
+        Args:
+            index_file (str, optional): Path to index file. Defaults to "./data/index_tf.pickle".
+            pca_file (str, optional): Path to PCA file. Defaults to "./data/pca_tf.pickle".
+        """
+        self.index_file = index_file
+        self.pca_file = pca_file
+        print("Loading model...")
+        self.model = keras.applications.VGG16(weights="imagenet", include_top=True)
+        self.feature_extractor = Model(inputs=self.model.input, 
+                                       outputs=self.model.get_layer("fc2").output)
+        print("Loading index...")
+        self.index = self.__load_index(self.index_file)
+        print("Loading PCA...")
+        self.pca = self.__load_pca(self.pca_file)
+
+    def __load_image(self, image_input: Union[str, Image.Image]) -> Tuple[Image.Image, np.ndarray]: 
+        """
+        Preprocesses image for VGG16 model
+
+        Args:
+            image_input (Union[str, Image.Image]): Path to image or PIL Image
+        
+        Returns:
+            Tuple[Image.Image, np.ndarray]: Image and preprocessed image
+        """
+        if type(image_input) != "str":
+            img = image_input.resize(self.model.input_shape[1:3])
+        else:
+            img = keras.utils.load_img(image_input, target_size=self.model.input_shape[1:3])
+        x = keras.utils.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        return img, x
+
+    def __extract_features(self, image_path: str) -> np.ndarray:
+        """
+        Extracts features from image (VGG16 fc2 layer)
+
+        Args:
+            image_path (str): Path to image
+
+        Returns:
+            np.ndarray: Features
+        """
+        img, x = self.__load_image(image_path)
+        features = self.feature_extractor.predict(x)
+        return features
+
+    def __train_pca(self, features: np.ndarray) -> PCA:
+        """
+        Train PCA on features (dimensionality reduction)
+
+        Args:
+            features (np.ndarray): Features
+
+        Returns:
+            PCA: PCA model
+        """
+        n = min(300, len(features))
+        pca = PCA(n_components=n)
+        pca.fit(features)
+        with open(self.pca_file, "wb") as f:
+            pickle.dump(pca, f)
+        return pca
+
+    def create_index(self, image_repo: str) -> None:
+        """
+        Creates index of images in image_repo and saves it to pickle file
+
+        Args:
+            image_repo (str): Path to image repository
+
+        Returns:
+            None
+        """
+        features = []
+        names = []
+        images = list(Path(image_repo).iterdir())
+        for i, image_path in enumerate(images):
+            print(f"Processing {image_path.name}, Length of index {len(features)}")
+            feat = self.__extract_features(image_path)[0]
+            features.append(feat)
+            names.append(image_path.name)          # might have different order than images
+
+        print("Extracting PCA features (might take some minutes)...")
+        self.pca = self.__train_pca(features)
+        pca_features = self.pca.transform(features)
+        self.index = [names, pca_features]
+
+        with open(self.index_file, "wb") as f:
+            print(f"Saving index to {self.index_file}...")
+            pickle.dump(self.index, f, protocol=pickle.HIGHEST_PROTOCOL)
+            gc.collect()            # garbage collection
+             
+    def __load_index(self, index_file: str) -> List:
+        """
+        Loads index from pickle file
+
+        Args:
+            index_file (str): Path to index file
+
+        Returns:
+            List: Index
+        """
+        if Path(index_file).exists() == False:
+            print(f"Index file {index_file} not found. Please use create_index().")  
+            return None
+        with open(index_file, "rb") as f:
+            index = pickle.load(f)
+            return index
+
+    def __load_pca(self, pca_file: str) -> PCA:
+        """
+        Loads PCA from pickle file
+
+        Args:
+            pca_file (str): Path to PCA file
+
+        Returns:
+            PCA: PCA model
+        """
+        if Path(pca_file).exists() == False:
+            print(f"PCA file {pca_file} not found. Please use create_index().")  
+            return None
+        with open(pca_file, "rb") as f:
+            pca = pickle.load(f)
+            return pca
+
+    def find_similar_images(self, image_path: str, top_k: int = 10) -> List[Tuple[str, float]]:
+        """
+        Finds similar images to image_path and returns top_k similar images, based on shortest distance
+
+        Args:
+            image_path (str): Path to image
+            top_k (int, optional): Number of similar images to return. Defaults to 10.
+
+        Returns:
+            List[Tuple[str, float]]: List of tuples with similar image (image_path, distance)
+        """
+        new_features = self.__extract_features(image_path)
+        new_pca_features = self.pca.transform(new_features)[0]
+        distances = [ distance.cosine(new_pca_features, feat) for feat in self.index[1] ]
+        idx_closest = sorted(range(len(distances)), key=lambda k: distances[k])[:top_k]
+        similar_images = list(zip([self.index[0][i] for i in idx_closest],
+                                  [distances[i] for i in idx_closest]))
+        return similar_images
+
+
+if __name__ == "__main__":
+    clf = KerasImageClassifier()
+    #clf.create_index("./img/imagenet-mini/")
+
+    # Test
+    uploaded_img = "./img/dog_input.jpg"
+    similar_images = clf.find_similar_images(uploaded_img)
+    
+    im = Image.open(uploaded_img)
+    similar_images = clf.find_similar_images(im)
+    print(similar_images)
+
+    # Display
+    im = Image.open(f"./img/imagenet-mini/{similar_images[0][0]}")
+    im.show()
+    print("Done")
